f76fef3dae8737570fa474ae9fe2b74a679796b9
Sergio Nezhigay <abc1971h@gmail.com>
2023-02-21 13:05:33 +0200

    task 1 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,34 @@
+const startButtonEl = document.querySelector('[data-start]');
+const stopButtonEl = document.querySelector('[data-stop]');
+const bodyEl = document.querySelector('body');
 
+startButtonEl.addEventListener('click', onStartClick);
+styleButtonsAsRunning(false);
+
+function onStartClick() {
+  intervalID = setInterval(() => {
+    changeBodyBgcolor(getRandomHexColor());
+  }, 1000);
+  stopButtonEl.addEventListener(
+    'click',
+    () => {
+      clearInterval(intervalID);
+      styleButtonsAsRunning(false);
+    },
+    { once: true }
+  );
+  styleButtonsAsRunning(true);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function changeBodyBgcolor(color) {
+  bodyEl.style.backgroundColor = color;
+}
+
+function styleButtonsAsRunning(isRunning) {
+  startButtonEl.disabled = isRunning ? true : false;
+  stopButtonEl.disabled = isRunning ? false : true;
+}
